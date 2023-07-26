c704e08daf5c263037c32acac6fea50fbeb3127a
Craig David <craig.david@mt.com>
2023-07-26 16:06:09 -0400

    small updates

--- src/routes/api/mixers/+server.js
+++ src/routes/api/mixers/+server.js
@@ -1,23 +1,16 @@
-import { mixersDb} from "$lib/server/forerunnerdb";
-import { io } from "$lib/webSocketConnection";
-import { json } from "@sveltejs/kit";
+import { mixersDb } from '$lib/server/forerunnerdb';
+import { json } from '@sveltejs/kit';
 
 let mixersColl = mixersDb.getCollection();
-
 
 /** @type {import('./$types').RequestHandler} */
 export async function GET() {
 	return json(mixersDb.getData());
 }
 
-
 export const POST = async ({ request }) => {
-    const body = await request.json();
-    //console.log(body)
-    //const allMixers  = body;
-   mixersColl.insert(body)
-   mixersDb.saveData();
-
-   io.send("mixer-update")
-    return json("Done", { status: 200 });
-}
+	const body = await request.json();
+	mixersColl.insert(body);
+	mixersDb.saveData();
+	return json('Done', { status: 200 });
+};
